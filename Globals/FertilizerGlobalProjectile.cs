cc664177e4e5113d0b845d3920f0262637f45083
Big E <evanmorrison42@gmail.com>
2026-09-09 09:55:21 -0400

    + Fertilizer now works on AA Classic trees

---
+++ Globals/FertilizerGlobalProjectile.cs
@@ -0,0 +1,40 @@
+﻿using AAModClassic._Content.Inferno.World.Tiles.Trees;
+using AAModClassic._Content.Mire.World.Tiles.Trees;
+using AAModClassic._Content.Void.World.Tiles.Trees;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAModClassic.Globals
+{
+    public class FertilizerGlobalProjectile : GlobalProjectile
+    {
+        public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) => entity.type == ProjectileID.Fertilizer;
+
+        public override void AI(Projectile projectile)
+        {
+            Point start = projectile.TopLeft.ToTileCoordinates();
+            Point end = projectile.BottomRight.ToTileCoordinates();
+
+            for (int x = start.X; x <= end.X; x++)
+            {
+                for (int y = start.Y; y <= end.Y; y++)
+                {
+                    if (!WorldGen.InWorld(x, y))
+                        continue;
+
+                    var t = Main.tile[x, y];
+
+                    if (t.TileType == ModContent.TileType<RazewoodSapling_Tile>() ||
+                        t.TileType == ModContent.TileType<RazewoodPalmSapling_Tile>() ||
+                        t.TileType == ModContent.TileType<BogwoodSapling_Tile>() ||
+                        t.TileType == ModContent.TileType<BogwoodPalmTreeSapling_Tile>() ||
+                        t.TileType == ModContent.TileType<OuroborosSapling_Tile>())
+                    {
+                        WorldGen.GrowTree(x, y);
+                    }
+                }
+            }
+        }
+    }
+}
