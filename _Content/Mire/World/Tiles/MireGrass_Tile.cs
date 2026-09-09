756f64a2df5f36bce9200d5bc4aaeeb591eabf93
Big E <evanmorrison42@gmail.com>
2026-09-08 22:17:39 -0400

    + AA Classic Grasses now properly spread to nearby tiles such as dirt or mud

--- _Content/Mire/World/Tiles/MireGrass_Tile.cs
+++ _Content/Mire/World/Tiles/MireGrass_Tile.cs
@@ -1,9 +1,9 @@
-using AAModClassic._Unreleased.Content.Mire.World.Tiles;
 using AAModClassic.UI.World;
+using AAModClassic.Utilities;
 using Microsoft.Xna.Framework;
-using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ObjectData;
 
 namespace AAModClassic._Content.Mire.World.Tiles
 {
@@ -12,20 +12,52 @@ namespace AAModClassic._Content.Mire.World.Tiles
         public override void SetStaticDefaults()
         {
             Main.tileSolid[Type] = true;
-            TileID.Sets.Conversion.Grass[Type] = true;
-            Main.tileBlendAll[Type] = true;
-            Main.tileMergeDirt[Type] = true;
             Main.tileBlockLight[Type] = true;
-            TileID.Sets.NeedsGrassFraming[Type] = true;
+            Main.tileBlendAll[Type] = true;
+
+            TileID.Sets.Conversion.Grass[Type] = true;
+            TileID.Sets.Grass[Type] = true;
+            TileID.Sets.CanBeDugByShovel[Type] = true;
             TileID.Sets.JungleSpecial[Type] = true;
+
+            Main.tileMergeDirt[Type] = true;        
+            TileID.Sets.NeedsGrassFraming[Type] = true;
+            
             DustType = ModContent.DustType<Dusts.AbyssiumDust>();
             AddMapEntry(new Color(0, 50, 140));
             RegisterItemDrop(ItemID.MudBlock);
+
+            if (TileObjectData.GetTileData(TileID.Sunflower, 0) is TileObjectData data && data.AnchorValidTiles != null)
+                data.AnchorValidTiles = [.. data.AnchorValidTiles, Type];
         }
 
         public override bool IsTileBiomeSightable(int i, int j, ref Color sightColor)
         {
             sightColor = Color.BlueViolet;
+            return true;
+        }
+
+        public override void RandomUpdate(int i, int j)
+        {
+            if (TileUtils.TrySpread(i, j, Type, 4, TileID.Mud) && Main.netMode != NetmodeID.SinglePlayer)
+                NetMessage.SendTileSquare(-1, i, j, 3, TileChangeType.None);
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num) => num = 3;
+
+        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            if (!effectOnly)
+            {
+                fail = true;
+                WorldGen.KillTile_MakeTileDust(i, j, Main.tile[i, j]);
+                Framing.GetTileSafely(i, j).TileType = TileID.Mud;
+            }
+        }
+
+        public override bool CanExplode(int i, int j)
+        {
+            WorldGen.KillTile(i, j, false, false, true);
             return true;
         }
     }
--- _Content/RedMushroom/World/Tiles/Mycelium_Tile.cs
+++ _Content/RedMushroom/World/Tiles/Mycelium_Tile.cs
@@ -1,5 +1,6 @@
 using AAModClassic._Content.RedMushroom.___PreHardmode.Items.Quest;
 using AAModClassic.UI.World;
+using AAModClassic.Utilities;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
@@ -11,11 +12,18 @@ namespace AAModClassic._Content.RedMushroom.World.Tiles
 	{
 		public override void SetStaticDefaults()
 		{
-			Main.tileSolid[Type] = true;
-            TileID.Sets.Conversion.Grass[Type] = true;
-            Main.tileBlendAll[Type] = true;
+            Main.tileSolid[Type] = true;
             Main.tileBlockLight[Type] = true;
+            Main.tileBlendAll[Type] = true;
+
+            //TileID.Sets.Conversion.Grass[Type] = true;
+            TileID.Sets.Grass[Type] = true;
+            TileID.Sets.CanBeDugByShovel[Type] = true;
+
+            TileID.Sets.NeedsGrassFramingDirt[Type] = TileID.Dirt;
             TileID.Sets.NeedsGrassFraming[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+
             DustType = ModContent.DustType<Dusts.MushDust>();
 			AddMapEntry(new Color(100, 100, 0));
             RegisterItemDrop(ItemID.DirtBlock);
@@ -23,6 +31,9 @@ namespace AAModClassic._Content.RedMushroom.World.Tiles
 
         public override void RandomUpdate(int i, int j)
         {
+            if (TileUtils.TrySpread(i, j, Type, 4, TileID.Dirt) && Main.netMode != NetmodeID.SinglePlayer)
+                NetMessage.SendTileSquare(-1, i, j, 3, TileChangeType.None);
+
             if (!Framing.GetTileSafely(i, j - 1).HasTile && Main.rand.NextBool(30))
             {
                 int style = Main.rand.Next(5);
@@ -53,6 +64,24 @@ namespace AAModClassic._Content.RedMushroom.World.Tiles
                 WorldGen.SquareTileFrame(x, y, true);
             }
             return false;
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num) => num = 3;
+
+        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            if (!effectOnly)
+            {
+                fail = true;
+                WorldGen.KillTile_MakeTileDust(i, j, Main.tile[i, j]);
+                Framing.GetTileSafely(i, j).TileType = TileID.Dirt;
+            }
+        }
+
+        public override bool CanExplode(int i, int j)
+        {
+            WorldGen.KillTile(i, j, false, false, true);
+            return true;
         }
     }
 }
